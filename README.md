815d2da3384a7351055dbf024f1d6e5de1a2561c
Gustavo Hayashida <gustavo.mec12@gmail.com>
2024-05-15 16:12:59 -0300

    projeto feito

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # conversor-temperatura
 Converter Temperaturas de Cº para Fº ou Kº
+Arquivo principal se chama main
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-#Arquivo principal
+qual_unidade = input('Você deseja converter para F ou para K? ')
 
-#Queria dormir
 
-#Evoluindo
+def c_para_f():
+    temp_f = float((temp_inserida * 1.8) + 32)
+    print(f'{temp_inserida}C é {temp_f}F')
+
+def c_para_k():
+    temp_k = float(temp_inserida + 273.15)
+    print(f'{temp_inserida}C é {temp_k}K')
+
+if qual_unidade == 'F':
+    temp_inserida = float(input('Digite a temperatura, em Célcius, que deseja converter: '))
+    c_para_f()
+elif qual_unidade == 'K':
+    temp_inserida = float(input('Digite a temperatura, em Célcius, que deseja converter: '))
+    c_para_k()
+else:
+    print('Unidade de medida não identificada. Tente novamente.')
